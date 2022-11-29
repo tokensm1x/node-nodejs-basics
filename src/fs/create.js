7c360348916eca22fe45c317df9c65f8b3ec68dd
tokensm1x <tokensmix@gmail.com>
2022-11-29 20:35:05 +0300

    feat: create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,32 @@
+import { stat, writeFile } from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const filePath = path.dirname(__filename) + "/files/fresh.txt";
+const text = "I am fresh and young";
+
 const create = async () => {
-    // Write your code here 
+    let isFileExists = false;
+    await stat(filePath)
+        .then(() => {
+            isFileExists = true;
+        })
+        .catch(() => {
+            isFileExists = false;
+        });
+
+    if (!isFileExists) {
+        writeFile(filePath, text)
+            .then(() => {
+                console.log("File created!");
+            })
+            .catch(() => {
+                throw new Error("Can't create file!");
+            });
+    } else {
+        throw new Error("File exists!");
+    }
 };
 
 await create();
