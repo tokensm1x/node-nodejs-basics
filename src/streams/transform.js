b3560f27d8db22a2f161d81cb1b8feccacd3dd7a
tokensm1x <tokensmix@gmail.com>
2022-11-30 22:30:11 +0300

    feat: transform stream

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,24 @@
+import { Transform, pipeline } from "stream";
+
 const transform = async () => {
-    // Write your code here 
+    try {
+        const readData = process.stdin;
+        const writeData = process.stdout;
+
+        const transformData = new Transform({
+            transform(chunk, enc, cb) {
+                const reversedString = chunk.toString().split("").reverse().join("");
+
+                cb(null, reversedString + "\n");
+            },
+        });
+
+        pipeline(readData, transformData, writeData, (err) => {
+            throw new Error("Filed to transform data!");
+        });
+    } catch (e) {
+        throw new Error("Filed to transform data!");
+    }
 };
 
 await transform();
