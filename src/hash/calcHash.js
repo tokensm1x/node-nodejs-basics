00f08a926153866b26abe30c2bdf0bb7fda4cdd9
tokensm1x <tokensmix@gmail.com>
2022-11-30 17:33:00 +0300

    feat: clac hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,32 @@
+import { createHash } from "crypto";
+import { createReadStream } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, "/files/fileToCalculateHashFor.txt");
+
 const calculateHash = async () => {
-    // Write your code here 
+    try {
+        const readStream = createReadStream(filePath);
+        let readContent = "";
+
+        readStream.on("data", (chunk) => {
+            readContent += chunk;
+        });
+
+        readStream.on("error", (err) => {
+            throw new Error("Calculation operation failed!");
+        });
+
+        readStream.on("end", () => {
+            const hash = createHash("sha256").update(readContent).digest("hex");
+            console.log(hash);
+        });
+    } catch (e) {
+        throw new Error("Calculation operation failed!");
+    }
 };
 
 await calculateHash();
