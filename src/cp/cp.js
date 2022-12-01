b5a0a6b1892d2b02b7a7716005a4c013f2e82575
tokensm1x <tokensmix@gmail.com>
2022-12-01 16:07:33 +0300

    feat: child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,15 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import { fork } from "child_process";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, "/files/script.js");
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const child = fork(filePath, args.slice(2));
+
+    child.on("exit", (code) => console.log(`Child process exited. Code: ${code}`));
 };
 
-spawnChildProcess();
+spawnChildProcess(process.argv);
