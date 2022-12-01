dd16a960c940a83ba05ebc9d960b0d36688c1eda
tokensm1x <tokensmix@gmail.com>
2022-12-01 17:00:55 +0300

    worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,39 @@
+import { cpus } from "os";
+import { Worker } from "worker_threads";
+const systemCpuCores = cpus();
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, "worker.js");
+
 const performCalculations = async () => {
-    // Write your code here
+    try {
+        let num = 10;
+        const workerPromises = [];
+        const createWorker = (number) => {
+            return new Promise((resolve, reject) => {
+                const worker = new Worker(filePath, { workerData: { num: number } });
+
+                worker.on("message", (result) => {
+                    resolve({ status: "resolved", data: result });
+                });
+
+                worker.on("error", (error) => {
+                    resolve({ status: "error", data: null });
+                });
+            });
+        };
+        systemCpuCores.forEach(() => {
+            workerPromises.push(createWorker(num++));
+        });
+
+        const result = await Promise.all(workerPromises);
+        console.log(result);
+    } catch (e) {
+        throw new Error(e);
+    }
 };
 
 await performCalculations();
