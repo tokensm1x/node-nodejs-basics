a9066412cc3a125b8e3ded69d32a009cbde162be
tokensm1x <tokensmix@gmail.com>
2022-12-01 14:10:30 +0300

    feat: args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
+const args = process.argv.slice(2);
+
 const parseArgs = () => {
-    // Write your code here 
+    const argsStrings = args.reduce((acc, el, i) => {
+        if (el.includes("--")) {
+            return (acc += `${el.replace("--", "")} is `);
+        } else {
+            return i === args.length - 1 ? (acc += el) : (acc += `${el}, `);
+        }
+    }, "");
+    console.log(argsStrings);
 };
 
 parseArgs();
